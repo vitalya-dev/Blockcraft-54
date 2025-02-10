4b8c09ac66d8600b8ef0724e21e549533ad11293
Vitalya Dev <vitalya.dev@gmail.com>
2025-02-10 17:38:36 +0300

    Working On.

--- TShape.js
+++ TShape.js
@@ -22,13 +22,14 @@ export default class TShape extends THREE.Group {
       box.tshape = this;
       
       // Create edges geometry for the box
-      // const edgesGeometry = new THREE.EdgesGeometry(geometry);
-      // const edgesMaterial = new THREE.LineBasicMaterial({ color: 0x000000 });
-      // const edges = new THREE.LineSegments(edgesGeometry, edgesMaterial);
-      // edges.tshape = this;
+      const edgesGeometry = new THREE.EdgesGeometry(geometry);
+      const edgesMaterial = new THREE.LineBasicMaterial({ color: 0x000000 });
+      const edges = new THREE.LineSegments(edgesGeometry, edgesMaterial);
+      edges.tshape = this;
+      edges.raycast = () => {};
       
       // Add the edges as a child of the box so that they follow its transform
-      //box.add(edges);
+      box.add(edges);
       
       // Add the box (with its edges) to the TShape group
       this.add(box);
